8a8a695738c103bc99df02fe6a719657f9b11ea4
tjlaud <62766674+tjlaud@users.noreply.github.com>
2020-12-06 17:38:52 +0000

    updated code from Al

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, TextField, Button } from "@material-ui/core";
 
 function App() {
+  const [battCap, setBattCap] = useState();
+  const [devCon, setDevCon] = useState();
+  const [answer, setAnswer] = useState("");
+
+  const updateBattCap = (event) => {
+    setBattCap(event.target.value);
+    console.log(battCap);
+  };
+  const updateDevCon = (event) => {
+    setDevCon(event.target.value);
+    console.log(devCon);
+  };
+
+  const calculate = () => {
+    let calcResult = 0;
+    if (battCap && devCon) {
+      calcResult = (battCap / devCon) * 0.7;
+    }
+    setAnswer(calcResult);
+  };
+
   return (
     <Box
       style={{
@@ -19,13 +40,27 @@ function App() {
     >
       <h1>Battery Life Calculator</h1>
       <h3>Battery Capacity</h3>
-      <TextField variant="standard" type="number" label="mHa" />
+      <TextField
+        value={battCap}
+        onChange={updateBattCap}
+        variant="standard"
+        type="number"
+        label="mHa"
+      />
       <h3>Device Consumption</h3>
-      <TextField variant="standard" type="number" label="mHa" />
+      <TextField
+        onChange={updateDevCon}
+        value={devCon}
+        variant="standard"
+        type="number"
+        label="mA"
+      />
       <br />
-      <Button variant="contained">Calculate</Button>
+      <Button onClick={calculate} variant="contained">
+        Calculate
+      </Button>
       <br />
-      <TextField disabled variant="filled" label="Hours" />
+      <TextField value={answer} disabled variant="filled" label="Hours" />
     </Box>
   );
 }
